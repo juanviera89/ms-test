c941a90d3859ec76cb63362b5f20e421e2ad2856
Juan Viera <jviera@marsol.cl>
2022-05-11 20:15:55 -0400

    Q1 ready and pause

--- index.js
+++ index.js
@@ -1,27 +1,39 @@
 const distance = (j,k) => {
     if( isNaN(j) || isNaN(k) ) throw new Error('params must be numbers')
-    if (k > j) throw new Error('k cant be greater than j');
+    if (k < j) throw new Error('j cant be greater than k');
 
     return k - j + 1
 }
 
 const maxJump = (blocks = [], initial = 0, inverse = false) => {
-
-    const jumpIndex = (inverse ? [...blocks].reverse() : blocks).findIndex( (block,i,_arr) => _arr.length = i+1 || block > _arr[i+1] )
-    return inverse ? blocks.length - jumpIndex +1 : jumpIndex ;
+    console.log('maxJump', blocks, initial, inverse);
+    const _arr =  (inverse ? [...blocks].reverse() : blocks).slice( inverse ? blocks.length - 1 - initial : initial, blocks.length)
+    // [ 0 1 2 3 4 5 6 7 8] = 9
+    // [ 8 7 6 5 4 3 2 1 0]
+    // pos inicial : 3 >> inverso : length - pos - 1 = 9 - 3 - 1 = 5
+    console.log('// find jump');
+    const jumpIndex = _arr.findIndex( (block,i,_arr2) => {
+        console.log(block, _arr2[i+1] ,i ,_arr2.length);
+        if (_arr2.length <= i+1) return true
+        return block > _arr2[i+1] 
+    })
+    const res = inverse ? initial - jumpIndex : initial + jumpIndex ;
+    console.log(jumpIndex,res);
+    return res
 
 }
 
 function solution(blocks = []) {
-    const distances =  blocks.map ( (block,j, _arr) => {
-        const j = maxJump(blocks,j,true), k = maxJump(blocks,j)
+    const distances =  blocks.map ( (block,i, _arr) => {
+        const j = maxJump(blocks,i,true), k = maxJump(blocks,i)
         const _distance = distance(j,k);
+        return _distance
     } )
     const {pos,maxDistance} = distances.reduce( ( _res, dis, j) => {
         if ( dis > _res.maxDistance) return {pos : j,maxDistance : dis}
         return _res
     }, {pos : 0,maxDistance : 0} )
-    console.log(pos,distance)
+    console.log(pos,maxDistance)
     return maxDistance
 }
 
